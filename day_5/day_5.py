4d128532e30f92cad6ca0a23e250d99fd2d86c55
aca0339 <allison.crim@str.us>
2022-12-05 12:00:35 -0500

    Solved day 5 AoC 2022

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,4 +1,7 @@
 #read from text file
+from re import L
+
+
 with open("data.txt") as f:
     contents = f.readlines()
 
@@ -6,8 +9,7 @@ with open("data.txt") as f:
 contents = [x.strip() for x in contents]  
 
 placement = contents[0:8]
-print(placement)
-directions = contents[11:512]
+directions = contents[10:512]
 #print(directions)
 
 col_1 = []
@@ -19,9 +21,10 @@ col_6 = []
 col_7 = []
 col_8 = []
 col_9 = []
-for items in placement:
+for items in reversed(placement):
+    #print(len(items))
     if(len(items)<32):
-        col_9.append(' ')
+        pass
     else:
         col_9.append(items[33])    
     col_1.append(items[1]) 
@@ -33,17 +36,34 @@ for items in placement:
     col_7.append(items[25]) 
     col_8.append(items[29])  
 
-print(list(reversed(col_1)))
-print(list(reversed(col_2)))
-print(list(reversed(col_3)))
-print(list(reversed(col_4)))
-print(list(reversed(col_5)))
-print(list(reversed(col_6)))
-print(list(reversed(col_7)))
-print(list(reversed(col_8)))
-print(list(reversed(col_9)))
+big_list =[col_1,col_2,col_3,col_4,col_5,col_6,col_7,col_8,col_9]
+
+for i in range(9):
+    remove_space = [x.strip(' ') for x in big_list[2]]
+    big_list[i] = [ele for ele in big_list[i] if ele.strip()]
+    
 
 for items in directions:
-    quantity = items[1]
-    init_loc = items[3]
-    final_loc = items[5]
+    items = items.split()
+    quantity = int(items[1])
+    print(quantity)
+    init_loc = int(items[3])-1
+    print(init_loc)
+    final_loc = int(items[5])-1
+    print(final_loc)
+
+    #for i in range(quantity):
+    #    big_list[final_loc].append(big_list[init_loc].pop())
+
+    print(big_list)
+    chunk = big_list[init_loc][-1*quantity:]
+    del big_list[init_loc][-1*quantity:]
+    print(chunk[0:3])
+    big_list[final_loc].extend(chunk[0:quantity])
+    #big_list[final_loc].append(big_list[init_loc].pop())
+
+final_list = []    
+for i in range(9):
+    final_list.append(big_list[i][-1])
+#print(big_list)
+print(final_list)
